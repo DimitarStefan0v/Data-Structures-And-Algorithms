2f481ff5b545e614ddbb688c1916dbd0de433c71
Dimitar Stefanov <dimitarnew9@gmail.com>
2023-05-02 16:47:51 +0300

    Add NestedLoopsToRecursion

---
+++ Algorithms/RecursionAndCombinatorial-Exercise/NestedLoopsToRecursion/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace NestedLoopsToRecursion
+{
+    internal class Program
+    {
+        private static int[] elements;
+
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+
+            elements = new int[n];
+
+            GenerateVectors(0);
+        }
+
+        private static void GenerateVectors(int index)
+        {
+            if (index >= elements.Length)
+            {
+                Console.WriteLine(string.Join(" ", elements));
+                return;
+            }
+
+            for (int i = 1; i <= elements.Length; i++)
+            {
+                elements[index] = i;
+                GenerateVectors(index + 1);
+            }
+        }
+    }
+}
