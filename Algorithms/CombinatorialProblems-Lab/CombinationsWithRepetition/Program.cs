271e8f167456814a5335b377976508c7499070f3
Dimitar Stefanov <dimitarnew9@gmail.com>
2023-05-02 15:41:50 +0300

    Add CombinationsWithRepetition

---
+++ Algorithms/CombinatorialProblems-Lab/CombinationsWithRepetition/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace CombinationsWithRepetition
+{
+    internal class Program
+    {
+        private static int k;
+        private static string[] elements;
+        private static string[] combinations;
+
+        static void Main(string[] args)
+        {
+            elements = Console.ReadLine().Split();
+            k = int.Parse(Console.ReadLine());
+
+            combinations = new string[k];
+
+            GetCombinations(0, 0);
+        }
+
+        private static void GetCombinations(int index, int elementsStartIndex)
+        {
+            if (index >= combinations.Length)
+            {
+                Console.WriteLine(string.Join(" ", combinations));
+                return;
+            }
+
+            for (int i = elementsStartIndex; i < elements.Length; i++)
+            {
+                combinations[index] = elements[i];
+                GetCombinations(index + 1, i);
+            }
+        }
+    }
+}
