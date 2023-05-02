c39c71f9d9a9e887bc45c7fb8c8626287e77be9d
Dimitar Stefanov <dimitarnew9@gmail.com>
2023-05-02 16:38:22 +0300

    Add Reverse Arr from exercise

---
+++ Algorithms/RecursionAndCombinatorialAlgorithms-Exercise/ReverseArr/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace ReverseArr
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var elements = Console.ReadLine().Split();
+
+            Reverse(elements, 0);
+        }
+
+        private static void Reverse(string[] elements, int index)
+        {
+            if (index == elements.Length / 2)
+            {
+                Console.WriteLine(string.Join(" ", elements));
+                return;
+            }
+
+            var temp = elements[index];
+            elements[index] = elements[elements.Length - index - 1];
+            elements[elements.Length - index - 1] = temp;
+
+            Reverse(elements, index + 1);
+        }
+    }
+}
