ee3bf40da8d7d91b20117d9dc68ed1f70b289eaf
Dimitar Stefanov <dimitarnew9@gmail.com>
2023-05-02 09:39:21 +0300

    refactor permutation algo

--- Algorithms/CombinatorialProblems-Lab/PermutationsWithoutRepetitions/Program.cs
+++ Algorithms/CombinatorialProblems-Lab/PermutationsWithoutRepetitions/Program.cs
@@ -3,38 +3,36 @@
     internal class Program
     {
         private static string[] elements;
-        private static string[] permutations;
-        private static bool[] used;
         static void Main(string[] args)
         {
             elements = new[] { "A", "B", "C" };
-            permutations = new string[elements.Length];
-            used = new bool[elements.Length];
 
             Permute(0);
         }
 
         private static void Permute(int index)
         {
-            if (index >= permutations.Length)
+            if (index >= elements.Length)
             {
-                Console.WriteLine(string.Join(" ", permutations));
+                Console.WriteLine(string.Join(" ", elements));
                 return;
             }
 
-            for (int i = 0; i < elements.Length; i++)
+            Permute(index + 1);
+
+            for (int i = index + 1; i < elements.Length; i++)
             {
-                if (used[i] == false)
-                {
-                    used[i] = true;
-                    permutations[index] = elements[i];
-
-                    Permute(index + 1);
-
-                    used[i] = false;
-                }
-
+                Swap(index, i);
+                Permute(index + 1);
+                Swap(index, i);
             }
+        }
+
+        private static void Swap(int first, int second)
+        {
+            var temp = elements[first];
+            elements[first] = elements[second];
+            elements[second] = temp;
         }
     }
 }
