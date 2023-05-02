31e50e34d1001eecfc7ab4198f8392a89a0bcf55
Dimitar Stefanov <dimitarnew9@gmail.com>
2023-05-02 09:24:15 +0300

    Add Permutations without Repetitions

---
+++ Algorithms/CombinatorialProblems-Lab/PermutationsWithoutRepetitions/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace PermutationsWithoutRepetitions
+{
+    internal class Program
+    {
+        private static string[] elements;
+        private static string[] permutations;
+        private static bool[] used;
+        static void Main(string[] args)
+        {
+            elements = new[] { "A", "B", "C" };
+            permutations = new string[elements.Length];
+            used = new bool[elements.Length];
+
+            Permute(0);
+        }
+
+        private static void Permute(int index)
+        {
+            if (index >= permutations.Length)
+            {
+                Console.WriteLine(string.Join(" ", permutations));
+                return;
+            }
+
+            for (int i = 0; i < elements.Length; i++)
+            {
+                if (used[i] == false)
+                {
+                    used[i] = true;
+                    permutations[index] = elements[i];
+
+                    Permute(index + 1);
+
+                    used[i] = false;
+                }
+
+            }
+        }
+    }
+}
