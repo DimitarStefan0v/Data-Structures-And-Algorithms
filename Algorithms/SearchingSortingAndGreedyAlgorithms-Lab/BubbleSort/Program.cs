8d4ec4a24d9e0759b30bdf5cef4154b196d0cb9a
Dimitar Stefanov <dimitarnew9@gmail.com>
2023-05-08 19:22:37 +0300

    Add BubbleSort

---
+++ Algorithms/SearchingSortingAndGreedyAlgorithms-Lab/BubbleSort/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace BubbleSort
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            BubbleSort(numbers);
+
+            Console.WriteLine(string.Join(" ", numbers));
+        }
+
+        private static void BubbleSort(int[] numbers)
+        {
+            var sortedCount = 0;
+            var isSorted = false;
+
+            while (isSorted == false)
+            {
+                isSorted = true;
+
+                for (int j = 1; j < numbers.Length - sortedCount; j++)
+                {
+                    var i = j - 1;
+                    if (numbers[i] > numbers[j])
+                    {
+                        Swap(numbers, i, j);
+                        isSorted = false;
+                    }
+                }
+
+                sortedCount++;
+            }
+        }
+
+        private static void Swap(int[] numbers, int first, int second)
+        {
+            var temp = numbers[first];
+            numbers[first] = numbers[second];
+            numbers[second] = temp;
+        }
+    }
+}
